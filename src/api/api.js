dc56fe5d9a6fd60ed2059eeb47a7418953ee1df5
Jacob Marsay <jacobmarsay@calls9.com>
2023-04-18 09:24:34 +0100

    constructing urls for games and their details

--- src/api/api.js
+++ src/api/api.js
@@ -20,3 +20,15 @@ const popular_games = `/games?key${process.env.API_KEY}&dates=${lastYear},${curr
 const upcoming_games = `/games?key=${process.env.API_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
 // get new games
 const new_games = `/games?key${process.env.API_KEY}&dates=${currentDate},${lastYear}&ordering=-released&page_size=10`;
+
+export const popularGamesUrl = () => `${base_url}${popular_games}`;
+export const upcomingGamesUrl = () => `${base_url}${upcoming_games}`;
+export const newgamesUrl = () => `${base_url}${new_games}`;
+// GAME DETAILS
+
+export const gameDetailsUrl = (game_id) =>
+  `${base_url}games/${game_id}?&key=${process.env.REACT_APP_KEY}`;
+
+  // GAME DETAILS SCREENSHOTS
+export const gameScreenshotUrl = (game_id) =>
+  `${base_url}games/${game_id}/screenshots?&key=${process.env.REACT_APP_KEY}`;
