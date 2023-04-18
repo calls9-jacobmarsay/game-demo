4e8529f012057bb89e243e694461713de8cd0af2
Jacob Marsay <jacobmarsay@calls9.com>
2023-04-18 13:16:10 +0100

    Updated game slice and added loadDeatils that fetches the games

--- src/store/slice/gamesSlice.js
+++ src/store/slice/gamesSlice.js
@@ -1,23 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { popularGamesUrl, upcomingGamesUrl, newgamesUrl, gameDetailsUrl, gameScreenshotUrl} from '../../api/api';
+import axios from 'axios';
 
-const initialState = {
-  popular: [],
-  newGames: [],
-  upcoming: [],
-  searched: [],
-};
+export const loadGames = createAsyncThunk('games/loadGames', async () => {
+  const popularData = await axios.get(popularGamesUrl());
+  const upcomingData = await axios.get(upcomingGamesUrl());
+  const newGameData = await axios.get(newgamesUrl());
 
-const gamesSlice = createSlice({
-  name: 'games',
-  initialState,
-  reducers: {
-    fetchGames(state, action) {
-      state.popular = action.payload.popular;
-      state.upcoming = action.payload.upcoming;
-      state.newGames = action.payload.newGames;
-    },
-  },
+  return {
+    popular: popularData.data.results,
+    upcoming: upcomingData.data.results,
+    newGames: newGameData.data.results,
+  };
+});
+  
+
+
+export const loadDetail = createAsyncThunk(async (id) => {
+    const detailData = await axios.get(gameDetailsUrl(id));
+    const screenshotData = await axios.get(gameScreenshotUrl(id));
+    return {
+      game: detailData.data,
+      screen: screenshotData.data,
+    };
 });
 
-export const { fetchGames } = gamesSlice.actions;
+const initialState = {
+    popular: [],
+    newGames: [],
+    upcoming: [],
+    searched: [],
+    game: {},
+    screen: {},
+    status: 'idle',
+    error: null,
+  };
+    
+  const gamesSlice = createSlice({
+    name: 'games',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+      builder.addCase(loadGames.fulfilled, (state, action) => {
+        state.popular = action.payload.popular;
+        state.upcoming = action.payload.upcoming;
+        state.newGames = action.payload.newGames;
+      });
+    },
+  });
+  
 export default gamesSlice.reducer;
