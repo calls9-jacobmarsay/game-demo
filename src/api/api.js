00dc0cbe380722c78bebb335e61e0b303f389485
Jacob Marsay <jacobmarsay@calls9.com>
2023-04-18 13:16:59 +0100

    Refactored the api, and solved typos

--- src/api/api.js
+++ src/api/api.js
@@ -1,25 +1,19 @@
+// BASE URL
+
 const base_url = "https://api.rawg.io/api/";
 
-// get date an convert to string
 const getDate = () => {
-    return new Date().toISOString().slice(0, 10);
-  };
+  return new Date().toISOString().slice(0, 10);
+};
 
-// get last year
-const lastYear = new Date().getFullYear() -1;
-
-// get next year
-const nextYear = new Date().getFullYear() +1;
-
-// get current date
+const lastYear = new Date().getFullYear() - 1;
+// const currentYear = new Date().getFullYear();
+const nextYear = new Date().getFullYear() + 1;
 const currentDate = getDate();
 
-// get popular games
-const popular_games = `/games?key${process.env.API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-// get upcoming games
-const upcoming_games = `/games?key=${process.env.API_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-// get new games
-const new_games = `/games?key${process.env.API_KEY}&dates=${currentDate},${lastYear}&ordering=-released&page_size=10`;
+const popular_games = `games?key=${process.env.REACT_APP_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?key=${process.env.REACT_APP_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const new_games = `games?key=${process.env.REACT_APP_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesUrl = () => `${base_url}${popular_games}`;
 export const upcomingGamesUrl = () => `${base_url}${upcoming_games}`;
@@ -28,7 +22,6 @@ export const newgamesUrl = () => `${base_url}${new_games}`;
 
 export const gameDetailsUrl = (game_id) =>
   `${base_url}games/${game_id}?&key=${process.env.REACT_APP_KEY}`;
-
-  // GAME DETAILS SCREENSHOTS
+// GAME DETAILS SCREENSHOTS
 export const gameScreenshotUrl = (game_id) =>
   `${base_url}games/${game_id}/screenshots?&key=${process.env.REACT_APP_KEY}`;
