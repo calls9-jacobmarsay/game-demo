141646e8a333115c06d594e39f2694426b3c945a
Jacob Marsay <jacobmarsay@calls9.com>
2023-04-18 17:06:36 +0100

    fixed console error

--- src/util.js
+++ src/util.js
@@ -1,4 +1,6 @@
 export const smallImage = (imagePath, size) => {
+  if (!imagePath) return null;
+
   const image = imagePath.match(/media\/screenshots/)
     ? imagePath.replace(
         "media/screenshots",
