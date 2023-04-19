2e3c36fcbe64696ba87c0f3ff9b70a08764fa2a0
Jacob Marsay <jacobmarsay@calls9.com>
2023-04-19 10:50:04 +0100

    Added classnames to div elements

--- src/pages/GameDetails.js
+++ src/pages/GameDetails.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { smallImage } from "../util";
+import '../styles/app.scss'
 
 const GameDetail = ({ pathId }) => {
   const { screen, game, isLoading } = useSelector((state) => state.games);
@@ -10,37 +11,44 @@ const GameDetail = ({ pathId }) => {
   }
 
   return (
-    <div>
-      <div>
-        <div>
-          <h3>{game.name}</h3>
-          <p>Rating: {game.rating}</p>
-        </div>
-        <div>
-          <h3>Platforms</h3>
-          <div>
-            {game.platforms?.map((data) => (
-              <p key={data.platform.id}>{data.platform.name}</p>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div>
-        <img src={smallImage(game.background_image, 1280)} alt="Game Art" />
-      </div>
-      <div>
-        <p>{game.description_raw}</p>
-      </div>
-      <div>
-        {screen.results?.map((screen) => (
-          <img
-            src={smallImage(screen.image, 1280)}
-            key={screen.id}
-            alt="Game Gallery"
-          />
-        ))}
-      </div>
-    </div>
+    	<div className="game__content">
+      	<div className="content">
+        		<div className="column">
+           		<div className="flex__row">
+               	<h2>{game.name}</h2>
+               	<p>Rating: {game.rating}</p>
+            	</div>
+            	<div className="game__description">
+                	<p><b>Description:</b> {game.description_raw}</p>
+            	</div>
+        		</div>
+        		<div className="column">
+            	<div className="preview_image">
+               	<img src={smallImage(game.background_image, 1280)} alt="Game Art" />
+            	</div>
+            	<div className="platforms">
+               	<h3>Platforms</h3>
+               	<div className="platform__content">
+                  	{game.platforms?.map((data) => (
+                  		<p key={data.platform.id}>{data.platform.name}</p>
+                  	))}
+               	</div>
+            	</div>
+        		</div>
+      	</div>
+      	<div className="gallery">
+				<h3>Gallery</h3>
+				<div className="gallery__image">
+					{screen.results?.map((screen) => (
+						<img
+							src={smallImage(screen.image, 1280)}
+							key={screen.id}
+							alt="Game Gallery"
+						/>
+					))}
+				</div>
+      	</div>
+		</div>
   );
 };
 
