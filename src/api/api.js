274106f9885311fc8388c8f5488ca256b6b9256a
Jacob Marsay <jacobmarsay@calls9.com>
2023-04-19 10:49:44 +0100

    change the requested number of games for each category from 10 to 12

--- src/api/api.js
+++ src/api/api.js
@@ -11,9 +11,9 @@ const lastYear = new Date().getFullYear() - 1;
 const nextYear = new Date().getFullYear() + 1;
 const currentDate = getDate();
 
-const popular_games = `games?key=${process.env.REACT_APP_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcoming_games = `games?key=${process.env.REACT_APP_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const new_games = `games?key=${process.env.REACT_APP_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const popular_games = `games?key=${process.env.REACT_APP_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=12`;
+const upcoming_games = `games?key=${process.env.REACT_APP_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=12`;
+const new_games = `games?key=${process.env.REACT_APP_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=12`;
 
 export const popularGamesUrl = () => `${base_url}${popular_games}`;
 export const upcomingGamesUrl = () => `${base_url}${upcoming_games}`;
